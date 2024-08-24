eba12f0d94f0f05a7b23b693f67bb4de5e7a544c
mibrahimmustafa <m.ibrahim.mustafa@gmail.com>
2024-08-25 01:35:30 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { SharedComponent } from './shared/shared.component';
+import { ButtonComponent } from './shared/button/button.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    ProductsComponent,
+    SharedComponent,
+    ButtonComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
